4861f6d31453149d43f49b071ce6d893444a610e
denis <denisdir72@gmail.com>
2018-07-17 16:34:14 +0500

    t140
    Show retail prices on the front end, product page, cart widget, cart, check out

---
+++ wp-content/plugins/ivcor-custom/includes/tasks/t130/assets/js/wcdpm-front-wc.js
@@ -0,0 +1,27 @@
+(function($){
+    if ($.tc_add_filter) {
+        $.tc_add_filter("tc_calculate_product_price", wcdpm_tc_calculate_product_price, 10, 1);
+
+        function wcdpm_tc_calculate_product_price(price) {
+            if (wcac) price = price + price * wcac.retailPriceProc / 100;
+            return price + price * wcdpm.retailPriceProc / 100;
+        }
+    }
+    $('.change-retail-price-proc-for-user').on('change', function(){
+        let value = $(this).val();
+        let userId = $(this).attr('user-id');
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: wcdpm.ajaxUrl,
+            data: {
+                action: 'change_retail_price_proc_for_user',
+                proc: value,
+                userId: userId
+            },
+            success: function (res) {
+                if (res) alert('Success!')
+            }
+        });
+    });
+})(jQuery);
