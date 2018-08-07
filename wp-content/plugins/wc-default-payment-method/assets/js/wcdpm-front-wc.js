b803373f5f4407bdf6bf2ff178889eac786de54e
denis <denisdir72@gmail.com>
2018-08-07 14:18:20 +0500

    remove old version plugins

--- wp-content/plugins/wc-default-payment-method/assets/js/wcdpm-front-wc.js
+++
@@ -1,6 +0,0 @@
-(function($){
-    $.tc_add_filter("tc_calculate_product_price", wcdpm_tc_calculate_product_price, 10, 1);
-    function wcdpm_tc_calculate_product_price(price) {
-        return price + price * wcdpm.retailPriceProc / 100;
-    }
-})(jQuery);
