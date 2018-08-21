dce9a06f980dbe3299b3fc6653ad681b5577ad0d
denis <denisdir72@gmail.com>
2018-08-21 17:19:08 +0500

    t203
    Bug in Choosing Shipping Method

--- wp-content/plugins/ivcor-custom/includes/tasks/t158/assets/js/front.js
+++ wp-content/plugins/ivcor-custom/includes/tasks/t158/assets/js/front.js
@@ -1,29 +1,40 @@
 (function($){
-    $('#ship-to-different-address-checkbox').on('change', function(){
-        if ($(this).prop('checked')) {
-            let k = 1;
-            $('ul#shipping_method li').each(function(){
-                if (k === 1) {
-                    $( document.body ).on('updated_checkout', function(){
-                        $('ul#shipping_method li:first-child').hide();
-                    });
-                } else if (k === 2) {
-                    $(this).find('input').prop('checked',1);
-                }
 
-                k++;
-            });
-        }else{
-            let k = 1;
-            $('ul#shipping_method li').each(function(){
-                if (k === 1) {
-                    $(this).find('input').prop('checked',1);
-                    $( document.body ).on('updated_checkout', function(){
-                        $('ul#shipping_method li:first-child').show();
-                    });
-                }
-                k++;
-            });
-        }
+    let currentShipingMethod = 1;
+    let k = 1;
+
+    $('#shipping_method .shipping_method').each(function(){
+        if ($(this).prop('checked')) currentShipingMethod = k;
+        k++;
     });
+
+    if (currentShipingMethod === 1) {
+        $('#ship-to-different-address-checkbox').on('change', function () {
+            if ($(this).prop('checked')) {
+                k = 1;
+                $('ul#shipping_method li').each(function () {
+                    if (k === 1) {
+                        $(document.body).on('updated_checkout', function () {
+                            $('ul#shipping_method li:nth-child(1)').hide();
+                        });
+                    } else if (k === 2) {
+                        $(this).find('input').prop('checked', 1);
+                    }
+
+                    k++;
+                });
+            } else {
+                k = 1;
+                $('ul#shipping_method li').each(function () {
+                    if (k === 1) {
+                        $(this).find('input').prop('checked', 1);
+                        $(document.body).on('updated_checkout', function () {
+                            $('ul#shipping_method li:nth-child(1)').show();
+                        });
+                    }
+                    k++;
+                });
+            }
+        });
+    }
 })(jQuery);
