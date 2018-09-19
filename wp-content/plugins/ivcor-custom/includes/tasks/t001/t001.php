ca65417be5f8582775ae93c59ba72c2d5c7da886
denis <denisdir72@gmail.com>
2018-09-19 11:10:29 +0500

    t206, t207
    Changes in Displaying Shipping Methods on Checkout Page

--- wp-content/plugins/ivcor-custom/includes/tasks/t001/t001.php
+++ wp-content/plugins/ivcor-custom/includes/tasks/t001/t001.php
@@ -15,7 +15,7 @@ if ( !isset($option) || !isset($option[$task]) || !$option[$task] ) {
  * hooks
  */
 add_filter('woocommerce_before_add_to_cart_form', 't001_woocommerce_before_add_to_cart_form', 25);
-//add_filter('woocommerce_dropdown_variation_attribute_options_args', 't001_woocommerce_dropdown_variation_attribute_options_args');
+add_filter('woocommerce_dropdown_variation_attribute_options_args', 't001_woocommerce_dropdown_variation_attribute_options_args');
 add_action('wp_enqueue_scripts', 't001_wp_enqueue_scripts' );
 /**
  * end hooks
@@ -26,15 +26,54 @@ function t001_woocommerce_before_add_to_cart_form() {
     if($product->get_cross_sell_ids()) {
 
         $header = '<div>' . _e('Choose a Material', 'woocommerce') . '';
+        /**
+         * t206
+         */
+        $material_cat = get_term_by('slug', 'material', 'product_cat' );
+        $materials_cat = get_term_children($material_cat->term_id, 'product_cat');
+
+        $category_ids = $product->get_category_ids();
+
+        $array_diff = array_intersect($materials_cat, $category_ids);
+
+        if (!empty($array_diff)) {
+            $cat_id = array_shift($array_diff);
+            $title = get_term($cat_id, 'product_cat')->name;
+        } else {
+            $title = $product->get_title();
+        }
+        /**
+         * end t206
+         */
+
         echo $header;
         echo '<select name="forma" onchange="location = this.value;">';
-        echo '<option disabled selected value>' . $product->get_title() . ' </option>';
+        echo '<option disabled selected value>' . $title . ' </option>';
 
         foreach ($product->get_cross_sell_ids() as $cross_sell_id) {
-            $cross_product = wc_get_product($cross_sell_id);
-            $url = get_permalink($cross_sell_id);
+            if (!is_array($cross_sell_id)) {
+                $cross_product = new WC_Product($cross_sell_id);
+                /**
+                 * t206
+                 */
 
-            echo '<option value="' . $url . '">' . $cross_product->get_title() . '</option>';
+                $category_ids = $cross_product->get_category_ids();
+
+                $array_diff = array_intersect($materials_cat, $category_ids);
+
+                if (!empty($array_diff)) {
+                    $cat_id = array_shift($array_diff);
+                    $title = get_term($cat_id, 'product_cat')->name;
+                } else {
+                    $title = $cross_product->get_title();
+                }
+                /**
+                 * end t206
+                 */
+                $url = get_permalink($cross_sell_id);
+
+                echo '<option value="' . $url . '">' . $title . '</option>';
+            }
 
         };
         echo '</select></div>';
